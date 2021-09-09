328ba3f854590d8ce45fdbcb20b2b0ca51e2bb3b
Lkhagva-erdene <anidas.com@gmail.com>
2021-09-09 11:26:17 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("hello Program");
+
+        //this is a comment and it doesnt effect my code!!!
+        //declaring variable x with value of 15
+        // int-> integer -> buhel too
+        int x = 15;
+        long lo = 15999999999l;
+
+        float f = 0.123456f;
+        double y = 5.12345678;
+
+        String abc = "%&$%&$%#^sd sg weg"; //String is a collection of characters
+        boolean asd = false; //it can store only two variables True or False
+        char a = '/';
+
+
+
+        System.out.print("x=");
+        System.out.println(x);
+
+    }
+}
---
+++ src/Second.java
@@ -0,0 +1,10 @@
+public class Second {
+
+    public static void main(String[] args){
+        int x = 15, y =20;
+
+
+        System.out.print("x+y=" +(x + y));
+
+    }
+}
